d4338f931c759f298aed2780ac5d06c5d64b15db
Doug Kulak <dougkulak@gmail.com>
2023-09-27 18:35:50 -0700

    updating CTAs, adding mobile fixes

--- src/components/ui/organisms/FreeConsultation/FreeConsultation.tsx
+++ src/components/ui/organisms/FreeConsultation/FreeConsultation.tsx
@@ -4,6 +4,7 @@ import { Button } from '@/components/ui/atoms/Button/Button.tsx';
 import { scrollToElementId } from '@/utils/scroll.ts';
 import { animationVariants } from '@/utils/animations.ts';
 import { AnimateOnScrollDiv } from '../../molecules/AnimateOnScrollDiv/AnimateOnScroll';
+import Image from 'next/image';
 
 export const FreeConsultation = () => {
   return (
@@ -12,8 +13,17 @@ export const FreeConsultation = () => {
         variants={animationVariants.fadeInUp}
         className="container flex flex-col items-center justify-center gap-8 lg:flex-row lg:gap-16"
       >
-        <h3 className={'text-center font-heading text-3xl text-white'}>
-          Schedule Your <span className={'font-black text-red'}>Free Consultation</span> Today
+        <h3 className={'text-center font-heading text-[28px] text-white'}>
+          <span className={'font-black text-white'}>Boost Your Business</span>
+          {' With '}
+          <Image
+            src={`/img/rocket-jones-logo-white.svg`}
+            alt={'Rocket Jones Interactive'}
+            width={212}
+            height={32}
+            className={`relative top-[-2px] mb-[3px] inline-block max-h-[32px] w-auto lg:max-h-[64px]`}
+            loading={'eager'}
+          />
         </h3>
         <Button variant={'primary'} onClick={() => scrollToElementId('contact')}>
           Schedule Now
--- src/components/ui/organisms/Hero/Hero.tsx
+++ src/components/ui/organisms/Hero/Hero.tsx
@@ -113,7 +113,7 @@ export const HeroDescription = () => {
 
 export const HeroCTA = ({ onOpenVideoClick }: { onOpenVideoClick: () => void }) => {
   return (
-    <div className={'flex gap-2'}>
+    <div className={'flex flex-col gap-2 sm:flex-row'}>
       <Button variant={'secondary'} onClick={() => scrollToElementId('contact')}>
         Free Consultation
       </Button>
