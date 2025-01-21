4615b4049617ef4dad7710daad1800e676357c39
Doug Kulak <doug.kulak@gmail.com>
2025-01-21 09:32:46 -0800

    adding GTM code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         }`,
           }}
         />
+        <Script id="google-tag-manager">
+          {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-TVPVTBS7');
+          `}
+        </Script>
         <Script src="https://www.googletagmanager.com/gtag/js?id=UA-5947153-1" />
         <Script id="google-analytics">
           {`
@@ -42,6 +51,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         </Script>
       </head>
       <body className={`overflow-x-hidden bg-white text-grey ${roboto.className}`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TVPVTBS7"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <Header />
         {children}
       </body>
