4a46f4ff1e27ae0a4d2ebb19af4dc7cb76157b3a
Doug Kulak <doug.kulak@gmail.com>
2025-01-21 10:27:06 -0800

    fixing zindex bug with header nav

--- src/components/ui/organisms/Header/Header.tsx
+++ src/components/ui/organisms/Header/Header.tsx
@@ -37,7 +37,7 @@ export const Header = () => {
     <div
       id={'header'}
       className={
-        'absolute bottom-0 left-0 right-0 top-0 z-20 mx-auto h-[var(--header-height)] px-4 py-2 transition-transform duration-0 lg:px-4 lg:py-4 ' +
+        'absolute bottom-0 left-0 right-0 top-0 z-30 mx-auto h-[var(--header-height)] px-4 py-2 transition-transform duration-0 lg:px-4 lg:py-4 ' +
         (isFixed
           ? '!fixed !top-[-130px] translate-y-[130px] bg-white text-black shadow-lg !duration-700 '
           : '') +
